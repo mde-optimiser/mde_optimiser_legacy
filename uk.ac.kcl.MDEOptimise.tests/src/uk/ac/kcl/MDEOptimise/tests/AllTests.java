8388d2fd4c953812e574180859eb481fe86fc4dd
Steffen Zschaler <szschaler@acm.org>
2016-03-13 18:03:59 +0000

    Added test suite combining all tests.

---
+++ uk.ac.kcl.MDEOptimise.tests/src/uk/ac/kcl/MDEOptimise/tests/AllTests.java
@@ -0,0 +1,11 @@
+package uk.ac.kcl.MDEOptimise.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({InterpreterTest.class, SyntaxTest.class})
+public class AllTests {
+
+}
--- uk.ac.kcl.MDEOptimise.tests/xtend-gen/uk/ac/kcl/MDEOptimise/tests/SyntaxTest.java
+++ uk.ac.kcl.MDEOptimise.tests/xtend-gen/uk/ac/kcl/MDEOptimise/tests/SyntaxTest.java
@@ -1,14 +1,21 @@
 package uk.ac.kcl.MDEOptimise.tests;
 
 import javax.inject.Inject;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipse.xtext.junit4.util.ParseHelper;
 import org.eclipse.xtext.junit4.validation.ValidationTestHelper;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.Extension;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.ac.kcl.MDEOptimiseInjectorProvider;
+import uk.ac.kcl.mDEOptimise.BasepathSpec;
+import uk.ac.kcl.mDEOptimise.EvolverSpec;
+import uk.ac.kcl.mDEOptimise.MetaModelSpec;
 import uk.ac.kcl.mDEOptimise.Optimisation;
 
 @InjectWith(MDEOptimiseInjectorProvider.class)
@@ -27,8 +34,37 @@ public class SyntaxTest {
    */
   @Test
   public void testBasicParsing() {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe method basepath is undefined for the type SyntaxTest"
-      + "\nlocation cannot be resolved");
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("basepath <ABC>");
+      _builder.newLine();
+      _builder.append("metamodel <ABC>");
+      _builder.newLine();
+      _builder.append("fitness \"ABC\"");
+      _builder.newLine();
+      _builder.append("evolve using <ABC>");
+      _builder.newLine();
+      _builder.append("evolve using <CDE>");
+      _builder.newLine();
+      final Optimisation model = this.parser.parse(_builder);
+      Assert.assertNotNull(model);
+      this._validationTestHelper.assertNoIssues(model);
+      BasepathSpec _basepath = model.getBasepath();
+      String _location = _basepath.getLocation();
+      Assert.assertEquals("ABC", _location);
+      MetaModelSpec _metamodel = model.getMetamodel();
+      String _location_1 = _metamodel.getLocation();
+      Assert.assertEquals("ABC", _location_1);
+      EList<EvolverSpec> _evolvers = model.getEvolvers();
+      EvolverSpec _get = _evolvers.get(0);
+      String _rule_location = _get.getRule_location();
+      Assert.assertEquals("ABC", _rule_location);
+      EList<EvolverSpec> _evolvers_1 = model.getEvolvers();
+      EvolverSpec _get_1 = _evolvers_1.get(1);
+      String _rule_location_1 = _get_1.getRule_location();
+      Assert.assertEquals("CDE", _rule_location_1);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
