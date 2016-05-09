09462988f9f6bea2486b0829416c799eecab6408
Steffen Zschaler <szschaler@acm.org>
2016-05-09 11:06:54 +0100

    Added support for generating unique synthetic class names.

--- uk.ac.kcl.MDEOptimise.tests/src/uk/ac/kcl/MDEOptimise/tests/ttc/implementation/CRASolving.xtend
+++ uk.ac.kcl.MDEOptimise.tests/src/uk/ac/kcl/MDEOptimise/tests/ttc/implementation/CRASolving.xtend
@@ -3,14 +3,16 @@ package uk.ac.kcl.MDEOptimise.tests.ttc.implementation
 import com.google.inject.Inject
 import java.text.SimpleDateFormat
 import java.util.Date
+import org.eclipse.emf.common.util.EList
+import org.eclipse.emf.ecore.EObject
 import org.eclipse.xtext.junit4.InjectWith
 import org.eclipse.xtext.junit4.XtextRunner
 import org.eclipse.xtext.junit4.util.ParseHelper
 import org.junit.Test
 import org.junit.runner.RunWith
 import uk.ac.kcl.MDEOptimise.tests.FullTestInjector
-import uk.ac.kcl.MDEOptimise.tests.models.DebuggableSimpleMO
 import uk.ac.kcl.interpreter.OptimisationInterpreter
+import uk.ac.kcl.interpreter.algorithms.SimpleMO
 import uk.ac.kcl.mDEOptimise.Optimisation
 
 @InjectWith(FullTestInjector)
@@ -33,11 +35,24 @@ class CRASolving {
 		''')
 
 		val modelProvider = new CRAModelProvider()
-
-		val interpreter = new OptimisationInterpreter(model, new DebuggableSimpleMO(50, 10, pathPrefix, modelProvider), modelProvider)
-
+		val interpreter = new OptimisationInterpreter(model, new SimpleMO(50, 10), modelProvider)
 		val optimiserOutcome = interpreter.execute()
+
+		// Ensure all classes have unique names
+		optimiserOutcome.map[cm | cm.getFeature("classes") as EList<EObject>].flatten.forEach[cl, i |
+			cl.setFeature ("name", "NewClass" + i)
+		]
 
 		modelProvider.storeModels (optimiserOutcome, pathPrefix + "/final")
 	}
+	
+	def getFeature(EObject o, String feature) {
+		o.eGet (o.eClass.getEStructuralFeature(feature))
+	}
+
+	def setFeature(EObject o, String feature, Object value) {
+		o.eSet (o.eClass.getEStructuralFeature(feature), value)
+	}
 }
+
+//		val interpreter = new OptimisationInterpreter(model, new DebuggableSimpleMO(50, 10, pathPrefix, modelProvider), modelProvider)
--- uk.ac.kcl.MDEOptimise.tests/xtend-gen/uk/ac/kcl/MDEOptimise/tests/ttc/implementation/CRASolving.java
+++ uk.ac.kcl.MDEOptimise.tests/xtend-gen/uk/ac/kcl/MDEOptimise/tests/ttc/implementation/CRASolving.java
@@ -1,21 +1,28 @@
 package uk.ac.kcl.MDEOptimise.tests.ttc.implementation;
 
+import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipse.xtext.junit4.util.ParseHelper;
 import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure2;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.ac.kcl.MDEOptimise.tests.FullTestInjector;
-import uk.ac.kcl.MDEOptimise.tests.models.DebuggableSimpleMO;
 import uk.ac.kcl.MDEOptimise.tests.ttc.implementation.CRAModelProvider;
 import uk.ac.kcl.interpreter.OptimisationInterpreter;
+import uk.ac.kcl.interpreter.algorithms.SimpleMO;
 import uk.ac.kcl.mDEOptimise.Optimisation;
 
 @InjectWith(FullTestInjector.class)
@@ -47,12 +54,34 @@ public class CRASolving {
       _builder.newLine();
       final Optimisation model = this.parser.parse(_builder);
       final CRAModelProvider modelProvider = new CRAModelProvider();
-      DebuggableSimpleMO _debuggableSimpleMO = new DebuggableSimpleMO(50, 10, pathPrefix, modelProvider);
-      final OptimisationInterpreter interpreter = new OptimisationInterpreter(model, _debuggableSimpleMO, modelProvider);
+      SimpleMO _simpleMO = new SimpleMO(50, 10);
+      final OptimisationInterpreter interpreter = new OptimisationInterpreter(model, _simpleMO, modelProvider);
       final Set<EObject> optimiserOutcome = interpreter.execute();
+      final Function1<EObject, EList<EObject>> _function = (EObject cm) -> {
+        Object _feature = this.getFeature(cm, "classes");
+        return ((EList<EObject>) _feature);
+      };
+      Iterable<EList<EObject>> _map = IterableExtensions.<EObject, EList<EObject>>map(optimiserOutcome, _function);
+      Iterable<EObject> _flatten = Iterables.<EObject>concat(_map);
+      final Procedure2<EObject, Integer> _function_1 = (EObject cl, Integer i) -> {
+        this.setFeature(cl, "name", ("NewClass" + i));
+      };
+      IterableExtensions.<EObject>forEach(_flatten, _function_1);
       modelProvider.storeModels(optimiserOutcome, (pathPrefix + "/final"));
     } catch (Throwable _e) {
       throw Exceptions.sneakyThrow(_e);
     }
+  }
+  
+  public Object getFeature(final EObject o, final String feature) {
+    EClass _eClass = o.eClass();
+    EStructuralFeature _eStructuralFeature = _eClass.getEStructuralFeature(feature);
+    return o.eGet(_eStructuralFeature);
+  }
+  
+  public void setFeature(final EObject o, final String feature, final Object value) {
+    EClass _eClass = o.eClass();
+    EStructuralFeature _eStructuralFeature = _eClass.getEStructuralFeature(feature);
+    o.eSet(_eStructuralFeature, value);
   }
 }
